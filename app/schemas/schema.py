aad6d0fcc03a5a2264af0a346789f42ea06b1ba2
KisaZehra-goML <137411918+KisaZehra-goML@users.noreply.github.com>
2023-10-14 13:50:59 +0530

    Update schema.py

--- app/schemas/schema.py
+++ app/schemas/schema.py
@@ -3,21 +3,23 @@ from pydantic import BaseModel, Field, json, Json
 # from pydantic.generics import GenericModel
 
 
-class UploadFileSchema(BaseModel):
-    user_id: Optional[int] = None
-    file_type: str
-    # folder_id: int
-    file_language: str
-    # file_path: str
-    # org_id : Optional[int] = None
-    class Config: 
-        orm_mode = True
+# class UploadFileSchema(BaseModel):
+#     user_id: Optional[int] = None
+#     file_type: str
+#     # folder_id: int
+#     file_language: str
+#     # file_path: str
+#     # org_id : Optional[int] = None
+#     class Config:
+#         orm_mode = True
 
-class UserIdFileIdSchema(BaseModel):
-    user_id: int
-    file_id: int
+class segementcreation(BaseModel):
+    project_id: int
+    user_id: str
+    parameters: dict
+    feature_selection:str
 
-class UpdateExtractionSchema(BaseModel):
-    extraction_id: int
-    question_id : int
-    user_answer: str
+# class UpdateExtractionSchema(BaseModel):
+#     extraction_id: int
+#     question_id : int
+#     user_answer: str
